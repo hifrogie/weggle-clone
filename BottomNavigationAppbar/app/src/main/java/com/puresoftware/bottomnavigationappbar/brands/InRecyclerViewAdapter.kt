2f2703e52c357e66e85d17d46c4a5179dc14d3de
DeepseaCorder <hanseong4248@gmail.com>
2023-01-11 19:14:58 +0900

    1. 네트워크 Uri를 가져와서 이미지 가져오기 다음: 해상도 및 Click등 세세한 거 수정, VIdeo 및 Image 불러오게하기

--- BottomNavigationAppbar/app/src/main/java/com/puresoftware/bottomnavigationappbar/brands/InRecyclerViewAdapter.kt
+++ BottomNavigationAppbar/app/src/main/java/com/puresoftware/bottomnavigationappbar/brands/InRecyclerViewAdapter.kt
@@ -1,9 +1,12 @@
 package com.puresoftware.bottomnavigationappbar.brands
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.puresoftware.bottomnavigationappbar.R
 import com.puresoftware.bottomnavigationappbar.databinding.HolderRecyclerviewInBinding
 
 class InRecyclerViewAdapter(context: Context, val itemList: MutableList<RecyclerInViewModel>) :
@@ -17,6 +20,13 @@ class InRecyclerViewAdapter(context: Context, val itemList: MutableList<Recycler
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
         val item = itemList[position]
+
+        val context = holder.itemView.context
+        Log.i("asdf", item.image)
+        holder.binding.ivBrandsItem.setImageResource(R.drawable.wegglecircle)
+        Glide.with(context).load("https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory&fname=https://k.kakaocdn.net/dn/EShJF/btquPLT192D/SRxSvXqcWjHRTju3kHcOQK/img.png")
+            .into(holder.binding.ivBrandsItem)
+
         holder.bind(item)
     }
 
