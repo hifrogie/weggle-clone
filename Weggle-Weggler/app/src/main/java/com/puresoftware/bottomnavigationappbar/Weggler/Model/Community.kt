fb0a819d88c2391de6377f2b24a506525c7ce7c1
김진우 <jinwoo3661@naver.com>
2023-03-02 02:47:52 +0900

    로그인 기능, 자동 로그인 구현, 토큰 저장, 위글러 기능

--- Weggle-Weggler/app/src/main/java/com/puresoftware/bottomnavigationappbar/Weggler/Model/Community.kt
+++
@@ -1,7 +0,0 @@
-package com.puresoftware.bottomnavigationappbar.Weggler.Model
-
-import java.io.Serializable
-
-class Community (
-    val content : ArrayList<CommunityContent>,
-):Serializable
--- Weggle-Weggler/app/src/main/java/com/puresoftware/bottomnavigationappbar/Weggler/Model/MultiCommunityMode.kt
+++
@@ -1,5 +0,0 @@
-package com.puresoftware.bottomnavigationappbar.Weggler.Model
-
-// 타입 모드
-const val type_joint = 1
-const val type_free = 2
