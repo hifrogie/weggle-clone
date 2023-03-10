b7eb10d6e87b9af1abe9258a6568297c104c6981
김진우 <jinwoo3661@naver.com>
2023-03-10 22:04:37 +0900

    Product (공동 구매 ) 추가 , Fragment 전환 시 데이터 전달 추가, model 수정

--- Weggle/app/src/main/java/com/puresoftware/bottomnavigationappbar/Weggler/Model/BodyReview.kt
+++ Weggle/app/src/main/java/com/puresoftware/bottomnavigationappbar/Weggler/Model/BodyReview.kt
@@ -2,6 +2,7 @@ package com.puresoftware.bottomnavigationappbar.Weggler.Model
 
 import java.io.Serializable
 
-class BodyProductForPOST(
-    val body: String
+//post review
+class BodyReview(
+    val body : MultiCommunityDataBody
 ) : Serializable
