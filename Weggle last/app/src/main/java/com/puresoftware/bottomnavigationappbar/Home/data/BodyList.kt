6e582d13afd6cc852aa70d3a086021009a491cbe
Lee Gyeong Eun <dltmfrlekdy1@naver.com>
2023-03-26 12:49:55 +0900

    카테고리

---
+++ Weggle last/app/src/main/java/com/puresoftware/bottomnavigationappbar/Home/data/BodyList.kt
@@ -0,0 +1,11 @@
+package com.puresoftware.bottomnavigationappbar.Home.data
+
+data class BodyList(val image:String,
+                    val name:String,
+                    val company:String,
+                    val charge:String,
+                    val discount:Int,
+                    val original:Int,
+                    val price:Int,
+                    val duration:String,
+                    val benefit:String)
--- Weggle last/app/src/main/java/com/puresoftware/bottomnavigationappbar/Home/manager/GroupManager.kt
+++ Weggle last/app/src/main/java/com/puresoftware/bottomnavigationappbar/Home/manager/GroupManager.kt
@@ -2,15 +2,18 @@ package com.puresoftware.bottomnavigationappbar.Home.manager
 
 import android.util.Log
 import com.puresoftware.bottomnavigationappbar.Home.GroupList
+import com.puresoftware.bottomnavigationappbar.Home.data.GroupBuyData
+import com.puresoftware.bottomnavigationappbar.Home.data.PagenationList
 import com.puresoftware.bottomnavigationappbar.Server.MasterApplication
 import com.puresoftware.bottomnavigationappbar.Weggler.Model.Product
+import com.puresoftware.bottomnavigationappbar.Weggler.Model.ProductList
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class GroupManager(private val wApp: MasterApplication) {
 
-    fun getGroup(name:String, paramFunc: (ArrayList<Product>) -> Unit){
+    fun getGroup(name:String, paramFunc: (ArrayList<GroupBuyData>) -> Unit){
         wApp.service.addGroupProduct(name)
             .enqueue(object : Callback<GroupList> {
                 override fun onResponse(call: Call<GroupList>, response: Response<GroupList>) {
@@ -18,6 +21,7 @@ class GroupManager(private val wApp: MasterApplication) {
 //                            var arrayProduct = ArrayList<String>()
 //                            arrayProduct.add(response.body()!!.productList[].subjectFiles.first())
                         paramFunc(response.body()!!.productList)
+                        Log.d("히히히히", response.errorBody().toString())
                     } else {
 //                        Toast.makeText(this,"광고를 가져오는데 실패하였습니다.",Toast.LENGTH_LONG).show()
                         Log.d("하하하ㅏㅎ", "하하하하")
@@ -30,5 +34,25 @@ class GroupManager(private val wApp: MasterApplication) {
 
             })
     }
+
+    //카테고리 product pagenation
+    fun getCategoryProduct(category:String,paramFunc:(ArrayList<GroupBuyData>)->Unit){
+        wApp.service.getCategoryProduct(category,0,10,listOf("id,ASC"))
+            .enqueue(object : Callback<PagenationList>{
+                override fun onResponse(call: Call<PagenationList>, response: Response<PagenationList>) {
+                    if (response.isSuccessful){
+                        response.body()?.let { paramFunc(it.content) }
+                    }else{
+                        Log.d("에러",response.errorBody()!!.string())
+                    }
+                }
+
+                override fun onFailure(call: Call<PagenationList>, t: Throwable) {
+                    Log.d("실패",t.toString())
+                }
+
+            })
+    }
+
 }
 
