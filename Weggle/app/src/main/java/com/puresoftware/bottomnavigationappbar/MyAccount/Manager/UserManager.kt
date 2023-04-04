7fc02580e2d1f02e4bf1a4604660985cef60d935
김진우 <jinwoo3661@naver.com>
2023-04-04 19:24:21 +0900

    공구해요 포스팅, 프로필 수정, 유저 ui, 유저 태그 등 기능 추가

--- Weggle/app/src/main/java/com/puresoftware/bottomnavigationappbar/MyAccount/Manager/UserManager.kt
+++ Weggle/app/src/main/java/com/puresoftware/bottomnavigationappbar/MyAccount/Manager/UserManager.kt
@@ -2,6 +2,9 @@ package com.puresoftware.bottomnavigationappbar.MyAccount.Manager
 
 import com.puresoftware.bottomnavigationappbar.Server.MasterApplication
 import com.puresoftware.bottomnavigationappbar.MyAccount.Model.User
+import com.puresoftware.bottomnavigationappbar.MyAccount.Model.UserBody
+import com.puresoftware.bottomnavigationappbar.MyAccount.Model.UserPatch
+import okhttp3.MultipartBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -50,6 +53,26 @@ class UserManager(
             .enqueue(object  : Callback<User>{
                 override fun onResponse(call: Call<User>, response: Response<User>) {
                     if (response.isSuccessful){
+                        paramFun(response.body(),null)
+                    }else{
+                        paramFun(null,response.errorBody()!!.string())
+                    }
+                }
+
+                override fun onFailure(call: Call<User>, t: Throwable) {
+                    paramFun(null,"error")
+                }
+
+            })
+    }
+
+    fun updateUserInfo(email:String?,PW:String?,newPW:String?,body:UserBody?
+                       ,paramFun: (User?, error: String?) -> Unit){
+        masterApp.service.updateUser(UserPatch(email,PW,newPW,body))
+            .enqueue(object : Callback<User>{
+                override fun onResponse(call: Call<User>, response: Response<User>) {
+                    if (response.isSuccessful){
+
                         paramFun(response.body(),null)
                     }else{
                         paramFun(null,response.errorBody()!!.string())
--- Weggle/app/src/main/java/com/puresoftware/bottomnavigationappbar/MyAccount/Model/UserBody.kt
+++ Weggle/app/src/main/java/com/puresoftware/bottomnavigationappbar/MyAccount/Model/UserBody.kt
@@ -4,12 +4,9 @@ import java.io.Serializable
 
 // 유저의 프로필 (body)
 
-class Profile (
-    var name : String?,
+class UserBody (
     var userComment : String?,
-    var userImage : String?,
-    var backgroundPic : String?,
-    var userKeyword : ArrayList<Int>?,
+    var userKeyword : ArrayList<String>?,
     var instagramUrl : String?, //instagram
     var blogUrl : String?, //blog
     var youtubeUrl : String? //youtube
